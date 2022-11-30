c2e591d64a643608f05f4f677c261f8433cfccd3
DohaEmad <102623555+DohaEmad@users.noreply.github.com>
2022-11-30 04:59:50 +0200

    CreditCard

---
+++ src/user/CreditCard.java
@@ -0,0 +1,24 @@
+package user;
+
+public class CreditCard {
+
+	double balance;
+	String cardNumber;
+	double getBalance()
+	{
+		return balance;
+	}
+	void setBalance(double balance)
+	{
+		this.balance=balance;
+	}
+	String getCardNumber()
+	{
+		return cardNumber;
+	}
+	void setCardNumber(String cardNumber)
+	{
+		this.cardNumber=cardNumber;
+	}
+	
+}
